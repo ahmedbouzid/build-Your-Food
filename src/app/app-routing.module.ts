a4f54bf2ac82ef4cd5f6ce3ea6f0321c784c9cc1
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-09-28 21:32:20 +0200

    -create food page componet -configure routing -add router link to navigate to food page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FoodComponent } from './food/food.component';
 
 const routes: Routes = [
   {path:'' , component : HomeComponent} ,
   {path:'search/:searchTerm' , component : HomeComponent},
-  {path:'tag/:tag' , component:HomeComponent}
+  {path:'tag/:tag' , component:HomeComponent},
+  {path : 'food/:id' , component : FoodComponent}
 ];
 
 @NgModule({
